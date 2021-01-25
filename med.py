dc05a2f84f5015c5d2835595ccf74ba120556730
li-lauren <laurenlidev@gmail.com>
2021-01-24 17:29:36 -0700

    Finish Check Detection

--- med.py
+++ med.py
@@ -34,3 +34,33 @@ def calc(s):
 
 print(calc("/ 6 - 4 2"))
 
+
+# CHECK DETECTION
+
+def check(king, queen):
+    """Given a chessboard with one K and one Q, see if the K can attack the Q.
+
+    This function is given coordinates for the king and queen on a chessboard.
+    These coordinates are given as a letter A-H for the columns and 1-8 for the
+    row, like "D6" and "B7":
+    """
+    k_col = king[0]
+    k_row = int(king[1])
+
+    q_col = queen[0]
+    q_row = int(queen[1])
+
+    # Same row or col check
+    if q_col == k_col or q_row == k_row:
+        return True
+
+    cols = {}
+    i = 1
+    for letter in "ABCDEFGH":
+        cols[letter] = i
+        i += 1
+
+    # Diagonal Check
+    return abs(cols[q_col] - cols[k_col]) == abs(q_row - k_row)
+
+print(check("D6", "H7"))
