f61f4595a72fe4deaadd149af9ef31b256ea6256
li-lauren <laurenlidev@gmail.com>
2021-01-25 13:54:11 -0700

    Finish Friends Check

--- med.py
+++ med.py
@@ -159,3 +159,118 @@ def dec2bin(num):
     return bin
 
 print(dec2bin(13))
+
+# Check Friends
+class PersonNode(object):
+    """A node in a graph representing a person.
+
+    This is created with a name and, optionally, a list of adjacent nodes.
+    """
+
+    def __init__(self, name, adjacent=[]):
+        self.name = name
+        self.adjacent = set(adjacent)
+
+    def __repr__(self):
+        return "<PersonNode %s>" % self.name
+
+class FriendGraph(object):
+    """Graph to keep track of social connections."""
+
+    def __init__(self):
+        """Create an empty graph.
+
+        We keep a dictionary to map people's names -> nodes.
+        """
+
+        self.nodes = {}
+
+    def add_person(self, name):
+        """Add a person to our graph.
+
+            >>> f = FriendGraph()
+            >>> f.nodes
+            {}
+
+            >>> f.add_person("Dumbledore")
+            >>> f.nodes
+            {'Dumbledore': <PersonNode Dumbledore>}
+        """
+
+        if name not in self.nodes:
+            # Be careful not to just add them a second time -- otherwise,
+            # if we accidentally added someone twice, we'd clear our their list
+            # of friends!
+            self.nodes[name] = PersonNode(name)
+
+    def set_friends(self, name, friend_names):
+        """Set two people as friends.
+
+        This is reciprocal: so if Romeo is friends with Juliet, she's
+        friends with Romeo (our graph is "undirected").
+
+        >>> f = FriendGraph()
+        >>> f.add_person("Romeo")
+        >>> f.add_person("Juliet")
+        >>> f.set_friends("Romeo", ["Juliet"])
+
+        >>> f.nodes["Romeo"].adjacent
+        {<PersonNode Juliet>}
+
+        >>> f.nodes["Juliet"].adjacent
+        {<PersonNode Romeo>}
+        """
+
+        person = self.nodes[name]
+
+        for friend_name in friend_names:
+            friend = self.nodes[friend_name]
+
+            # Since adjacent is a set, we don't care if we're adding duplicates ---
+            # it will only keep track of each relationship once. We do want to
+            # make sure that we're adding both directions for the relationship.
+            person.adjacent.add(friend)
+            friend.adjacent.add(person)
+
+    def are_connected(self, name1, name2):
+        """Is this name1 friends with name2?"""
+
+        seen = set()
+        to_visit = []
+
+        friend1 = self.nodes[name1]
+        friend2 = self.nodes[name2]
+
+        for node in friend1.adjacent:
+            to_visit.append(node)
+
+        while to_visit:
+            person = to_visit.pop(0)
+            if person is not friend2:
+                seen.add(person)
+                for per in person.adjacent:
+                    if per not in seen:
+                        to_visit.append(per)
+            else:
+                return True
+
+        return False
+
+f = FriendGraph()
+f.add_person("Frodo")
+f.add_person("Sam")
+f.add_person("Gandalf")
+f.add_person("Merry")
+f.add_person("Pippin")
+f.add_person("Treebeard")
+f.add_person("Sauron")
+f.add_person("Dick Cheney")
+
+f.set_friends("Frodo", ["Sam", "Gandalf", "Merry", "Pippin"])
+f.set_friends("Sam", ["Merry", "Pippin", "Gandalf"])
+f.set_friends("Merry", ["Pippin", "Treebeard"])
+f.set_friends("Pippin", ["Treebeard"])
+f.set_friends("Sauron", ["Dick Cheney"])
+
+print(f.are_connected("Frodo", "Sam"))
+
